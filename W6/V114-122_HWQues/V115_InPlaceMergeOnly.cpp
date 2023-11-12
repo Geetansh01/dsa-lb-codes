b35b137ebf862c0fa58b471aa79c90de57aef92c
Geetansh <geetansh.bhardwaj04@gmail.com>
2023-11-13 03:39:54 +0530

    L115 In-Place Merge Sort done

---
+++ W6/V114-122_HWQues/V115_InPlaceMergeOnly.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+//Merging two sorted arrays In-Place. 
+//TC : O(log (n+m)) * O(n+m)
+//SC : O(1)
+
+void InPlaceMergeSortedArrays(int* A, int sA, int* B, int sB){
+    int n = sA;
+    int m = n + sB;
+    int gap = (n+m)/2 + (n+m)%2;
+
+    while(gap >= 1){
+        int i = 0; 
+        int j = i+gap;
+        while(j < m){
+            if(i < n){
+                if(j < n){
+                    if(A[i] > A[j]){
+                        swap(A[i], A[j]);
+                    }
+                }
+                else{
+                    int tempj = j - n;
+                    if(A[i] > B[tempj]){
+                        swap(A[i], B[tempj]);
+                    }
+                }
+            }
+            else{
+                int tempi = i-n;
+                int tempj = j-n;
+                if(B[tempi] > B[tempj]){
+                    swap(B[tempi], B[tempj]);   
+                }
+            }
+
+            i++; 
+            j++;
+        }
+        
+        if(gap == 1){
+            return;
+        }
+        gap = (gap/2) + (gap%2);
+    }
+
+    return;
+}
+
+int main(){
+    int ODD[] = {1,3,5,7};
+    int sODD = 4;
+    int EVEN[] = {2,4,6,7,8,8,8,10,12,14,16,18};
+    int sEVEN = 12;
+
+    InPlaceMergeSortedArrays(ODD, sODD, EVEN, sEVEN);
+
+    cout<<"After in place Merging : "<<endl;
+    for(int i = 0; i < sODD; i++){
+        cout<<ODD[i]<<" ";
+    }
+    cout<<endl;
+    for(int j = 0; j < sEVEN; j++){
+        cout<<EVEN[j]<<" ";
+    }
+
+
+   
+return 0;
+}
