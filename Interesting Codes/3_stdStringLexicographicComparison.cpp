3f60cb3ad91cdfa96a7cdb21947e0453b3386825
Geetansh <145845798+Geetansh01@users.noreply.github.com>
2025-06-26 11:51:29 +0530

    replaced C style strings with C++ Strings

--- Interesting Codes/3_stdStringLexicographicComparison.cpp
+++ Interesting Codes/3_stdStringLexicographicComparison.cpp
@@ -4,11 +4,20 @@ using namespace std;
 //std::strings are compared lexicographically if you use Relational Operators (<, >, <=, >=)
 
 int main(){
-   cout<<("30" <= "30")<<endl;
-   cout<<("1" < "2")<<endl;
-   cout<<("13" < "23")<<endl;
-   cout<<("30" < "3")<<endl;
+   cout<<(string("30") <= string("30"))<<endl;  // 1 (i.e true)
+   cout<<(string("1") < string("2"))<<endl;     // 1 (i.e true)
+   cout<<(string("13") < string("23"))<<endl;   // 1 (i.e true)
+   cout<<(string("30") < string("3"))<<endl;    // 0 (i.e false)
+   
+   /*
+   Note: 
+   (i) Directly writing "30" will lead to unexpected behaviour as this is not a C++ String, it's C-style string {array of characters}
+   (ii) So, writing "30" < "3", you are comparing character pointers (i.e memory addresses), the result could be 0 or 1 (i.e Unpredictable)
+   */
+  //For Ex: 
+  
+    cout<<(string("30") < string("3"))<<endl;    // 0 (Correct and expected according to lexicographic comparison ✅ )
+    cout<<("30"<"3")<<endl; // may give 0 or 1 (Compiler dependent, wrong coding practice ❌)
 
-   //Output for all cases will be 1
 return 0;
 }
